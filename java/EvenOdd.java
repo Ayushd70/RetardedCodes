b4a71038374f0a37504aff80c4647fc1fb3cb286
Ayush Dubey <ayushdubey70@gmail.com>
2018-08-29 15:02:34 +0530

    java:add Odd or even program

---
+++ java/EvenOdd.java
@@ -0,0 +1,18 @@
+//This java program finds if a number is odd or even
+import java.util.Scanner;
+ 
+class EvenOdd
+{
+  public static void main(String args[])
+  {
+    int c;
+    System.out.println("Input an integer");
+    Scanner in = new Scanner(System.in);
+    c = in.nextInt();
+ 
+    if ( (c/2)*2 == c )
+      System.out.println("Even");
+    else
+      System.out.println("Odd");
+  }
+}
