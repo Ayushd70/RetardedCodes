999e7998d8335033a8f1e6a94febf9732463c907
Ayush Dubey <ayushdubey70@gmail.com>
2018-08-30 14:36:16 +0530

    java:add program for calculating factorial of large numbers

---
+++ java/BigFactorial.java
@@ -0,0 +1,25 @@
+//Java program for calculating factorial of large numbers
+import java.util.Scanner;
+import java.math.BigInteger;
+ 
+class BigFactorial
+{
+  public static void main(String args[])
+  {
+    int n, c;
+    BigInteger inc = new BigInteger("1");
+    BigInteger fact = new BigInteger("1");
+ 
+    Scanner input = new Scanner(System.in);
+ 
+    System.out.println("Input an integer");
+    n = input.nextInt();
+ 
+    for (c = 1; c <= n; c++) {
+      fact = fact.multiply(inc);
+      inc = inc.add(BigInteger.ONE);
+    }
+ 
+    System.out.println(n + "! = " + fact);
+  }
+}
