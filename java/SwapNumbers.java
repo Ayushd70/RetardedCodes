8619a878bc4d2487d7b3e556f46efec4f0870925
Ayush Dubey <ayushdubey70@gmail.com>
2018-08-31 14:09:11 +0530

    java: add  program to swap two numbers using a temporary variable. To swap numbers without using extra variable

---
+++ java/SwapNumbers.java
@@ -0,0 +1,23 @@
+//Java program to swap two numbers using a temporary variable. To swap numbers without using extra variable 
+import java.util.Scanner;
+ 
+class SwapNumbers
+{
+   public static void main(String args[])
+   {
+      int x, y, temp;
+      System.out.println("Enter x and y");
+      Scanner in = new Scanner(System.in);
+ 
+      x = in.nextInt();
+      y = in.nextInt();
+ 
+      System.out.println("Before Swapping\nx = "+x+"\ny = "+y);
+ 
+      temp = x;
+      x = y;
+      y = temp;
+ 
+      System.out.println("After Swapping\nx = "+x+"\ny = "+y);
+   }
+}
