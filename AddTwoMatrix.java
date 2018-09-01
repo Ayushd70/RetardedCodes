9e70763ae7d2c0d39aa115a00b8a882e652b88ab
Ayush Dubey <ayushdubey70@gmail.com>
2018-09-01 16:23:51 +0530

    java:add program to add matrix

---
+++ AddTwoMatrix.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+ 
+class AddTwoMatrix
+{
+   public static void main(String args[])
+   {
+      int m, n, c, d;
+      Scanner in = new Scanner(System.in);
+ 
+      System.out.println("Enter the number of rows and columns of matrix");
+      m = in.nextInt();
+      n  = in.nextInt();
+ 
+      int first[][] = new int[m][n];
+      int second[][] = new int[m][n];
+      int sum[][] = new int[m][n];
+ 
+      System.out.println("Enter the elements of first matrix");
+ 
+      for (  c = 0 ; c < m ; c++ )
+         for ( d = 0 ; d < n ; d++ )
+            first[c][d] = in.nextInt();
+ 
+      System.out.println("Enter the elements of second matrix");
+ 
+      for ( c = 0 ; c < m ; c++ )
+         for ( d = 0 ; d < n ; d++ )
+            second[c][d] = in.nextInt();
+ 
+      for ( c = 0 ; c < m ; c++ )
+         for ( d = 0 ; d < n ; d++ )
+             sum[c][d] = first[c][d] + second[c][d];  //replace '+' with '-' to subtract matrices
+ 
+      System.out.println("Sum of entered matrices:-");
+ 
+      for ( c = 0 ; c < m ; c++ )
+      {
+         for ( d = 0 ; d < n ; d++ )
+            System.out.print(sum[c][d]+"\t");
+ 
+         System.out.println();
+      }
+   }
+}
