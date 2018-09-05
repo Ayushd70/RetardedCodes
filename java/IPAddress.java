a367a919854b4ee54b9663970ff9da17a9f4ae25
Ayush Dubey <ayushdubey70@gmail.com>
2018-09-05 14:30:48 +0530

    java:add program to print local ip address

---
+++ java/IPAddress.java
@@ -0,0 +1,10 @@
+//This program prints IP or internet protocol address of your computer system. InetAddress class of java.net package is used, getLocalHost method returns InetAddress object which represents local host
+import java.net.InetAddress;
+ 
+class IPAddress
+{
+   public static void main(String args[]) throws Exception
+   {
+      System.out.println(InetAddress.getLocalHost());
+   }
+}
