0dca45a3043cf9f2a0943f6abde700e08808dec2
Ayush Dubey <ayushdubey70@gmail.com>
2018-09-05 14:32:46 +0530

    java:add program to open NotePad

---
+++ java/Notepad.java
@@ -0,0 +1,16 @@
+//Java program to open Notepad: Notepad, a text editor is installed in Windows operating system. It is used for creating and editing text files
+import java.util.*;
+import java.io.*;
+ 
+class Notepad {
+  public static void main(String[] args) {
+    Runtime rs = Runtime.getRuntime();
+ 
+    try {
+      rs.exec("notepad");
+    }
+    catch (IOException e) {
+      System.out.println(e);
+    }   
+  }
+}
