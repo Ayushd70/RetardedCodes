252b0501d321cc8eedf9e5973092421840cc65a3
Ayush Dubey <ayushdubey70@gmail.com>
2018-09-11 14:45:40 +0530

    c:add program for linear search
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ c/linsearch.c
@@ -0,0 +1,40 @@
+//C program for linear search
+#include <stdio.h>
+ 
+long linear_search(long [], long, long);
+ 
+int main()
+{
+   long array[100], search, c, n, position;
+ 
+   printf("Input number of elements in array\n");
+   scanf("%ld", &n);
+ 
+   printf("Input %d numbers\n", n);
+ 
+   for (c = 0; c < n; c++)
+      scanf("%ld", &array[c]);
+ 
+   printf("Input number to search\n");
+   scanf("%ld", &search);
+ 
+   position = linear_search(array, n, search);
+ 
+   if (position == -1)
+      printf("%d isn't present in the array.\n", search);
+   else
+      printf("%d is present at location %d.\n", search, position+1);
+ 
+   return 0;
+} 
+ 
+long linear_search(long a[], long n, long find) {
+   long c;
+ 
+   for (c = 0 ;c < n ; c++ ) {
+      if (a[c] == find)
+         return c;
+   }
+ 
+   return -1;
+}
