17920c4de7cd317d441c52c50af46c0f039132df
Ayush Dubey <ayushdubey70@gmail.com>
2019-06-15 12:37:11 +0530

    c: add program for diffrent function
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ c/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ printf("hello world!\n");
+ printf("%d \n",32);/// integer value
+ printf("%ld \n",1231263213);/// large integer value
+ printf("%f \n",32.2);/// float value
+ printf("%lf \n",3222232324.211232131);/// large float value
+ printf("%c \n",'a');///character  value
+ printf("%s \n","hello world!");/// string value
+ printf("%x \n",16); ///hexadecimal value
+ return 0;
+}
