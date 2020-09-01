c661c7b2639754a5e78acc8d7590053966ef1f61
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-01 21:47:32 +0530

    go: values: add program to show diff types of values in go
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ go/values/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Adding a string with +
+	fmt.Println("go" + "lang")
+
+	//Int Type
+	fmt.Println("1+1 =", 1+1)
+
+	//Float Type
+	fmt.Println("5.0/2.0 =", 5.0/2.0)
+
+	//Booleans
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
