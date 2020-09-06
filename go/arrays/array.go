eedca09e624e0d6f9866b464a84ca58bbaa10fe8
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-06 21:54:41 +0530

    go: switch case: add a program for arrays
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ go/arrays/array.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	//Array of 5 elements
+	var a [5]int
+	fmt.Println("array", a)
+
+	//Setting a value to the 5th index in an array
+	a[4] = 5
+	fmt.Println("Array:", a)
+	fmt.Println("value", a[4])
+
+	//Length of an array
+	fmt.Println("len:", len(a))
+
+	//Declaring an array in a single line
+	b := [5]int{1,2,3,4,5}
+	fmt.Println("declare:", b)
+
+	//2-D Array
+	var twoD [4][5]int
+	for i := 0; i<4; i++ {
+		for j := 0; j<5; j++ {
+			twoD[i][j] = i+j
+		}
+	}
+	fmt.Println("2d Array:", twoD)
+
+}
