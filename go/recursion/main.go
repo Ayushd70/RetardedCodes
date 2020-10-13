d2eca75db26cce1285505e70d4ba75b147db45c2
Ayush Dubey <ayushdubey70@gmail.com>
2020-10-13 22:57:15 +0530

    go: add program for Recursion
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ go/recursion/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//call the function until it reaches the base case of fact(0)
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+}
