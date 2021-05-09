4eea21e969c205d9dfa433494fe3e2789b392fb9
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-09 19:14:43 +0530

    python: add program to divide two number
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/div.py
@@ -0,0 +1,10 @@
+//Python Program to divide two  numbers
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Divide two numbers
+div = float(num1) / float(num2)
+
+# Display the result
+print('The div of {0} and {1} is {2}'.format(num1, num2, div))
