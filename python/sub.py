e6ae477d96c25bdda0fc1e6bd38da9d790cab4d1
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-09 19:11:24 +0530

    python: add program to subtract two number
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/sub.py
@@ -0,0 +1,10 @@
+//Python Program to subtract two  numbers
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Sub two numbers
+sub = float(num1) - float(num2)
+
+# Display the sub
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sub)
