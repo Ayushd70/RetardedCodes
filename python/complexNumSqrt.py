b322439a022aaeba632ff7c9665d3ca0ab23a702
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-15 18:36:56 +0530

    python: add program to find square root of a complex number
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/complexNumSqrt.py
@@ -0,0 +1,13 @@
+# Find square root of real or complex numbers
+
+# Importing the complex math module
+import cmath
+
+num = eval(input("Enter a number: "))
+
+num_sqrt = cmath.sqrt(num)
+print(
+    "The square root of {0} is {1:0.3f}+{2:0.3f}j".format(
+        num, num_sqrt.real, num_sqrt.imag
+    )
+)
