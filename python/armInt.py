038cd97a9edbf660c4f190ad45f36950b5ebb1ee
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-16 20:18:44 +0530

    python: add program to check for Armstrong Number in a given interval
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/armInt.py
@@ -0,0 +1,21 @@
+# Program to check Armstrong numbers in a certain interval
+
+lower = int(input("Enter a lower bound: "))
+upper = int(input("Enter a upper bound: "))
+
+for num in range(lower, upper + 1):
+
+   # order of number
+   order = len(str(num))
+    
+   # initialize sum
+   sum = 0
+
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
