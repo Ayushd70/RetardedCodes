deaf48fec4d600874cafbc3b79622a237b81ecf8
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-16 20:16:28 +0530

    python: add python program to check Armstrong Number
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/armstrong.py
@@ -0,0 +1,23 @@
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# Changed num variable to string, and calculated the length (number of digits)
+order = len(str(num))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
