33a77e4fff634dbf396941588e328199edb68a24
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-16 20:08:25 +0530

    python: add program to print multiplication table
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/multiTable.py
@@ -0,0 +1,8 @@
+# Program to print Multiplication table (from 1 to 10) in Python
+
+# To take input from the user
+num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
