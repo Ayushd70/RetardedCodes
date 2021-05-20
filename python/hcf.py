6193ed15e7e3ff95a8725cc28559fe68775aee89
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-20 19:03:39 +0530

    python: add program to find the hcf of two numbers
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/hcf.py
@@ -0,0 +1,15 @@
+# Python Program to find the HCF of two Numbers
+
+def hcf(x, y):  
+   if x > y:  
+       smaller = y  
+   else:  
+       smaller = x  
+   for i in range(1,smaller + 1):  
+       if((x % i == 0) and (y % i == 0)):  
+           hcf = i  
+   return hcf  
+  
+num1 = int(input("Enter first number: "))  
+num2 = int(input("Enter second number: "))  
+print("The H.C.F. of", num1,"and", num2,"is", hcf(num1, num2))
