b7a571d03f621f53924c10f3b375004341f8f4c4
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-21 18:31:01 +0530

    python: add program to find the sum using recursion
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/sumrec.py
@@ -0,0 +1,14 @@
+# Python program to find the sum of natural using recursion
+
+num = int(input("Enter the number:"))
+
+def recur_sum(n):
+   if n <= 1:
+       return n
+   else:
+       return n + recur_sum(n-1)
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   print("The sum is",recur_sum(num))
