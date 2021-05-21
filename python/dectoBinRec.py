13101d1114c63fbdce5f5c8dca57e92645a060e2
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-21 18:35:30 +0530

    python: add program to convert decimal into binary using recursion
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/dectoBinRec.py
@@ -0,0 +1,11 @@
+# Function to print binary number using recursion
+
+dec = int(input("Enter the number:"))
+
+def convertToBinary(n):
+   if n > 1:
+       convertToBinary(n//2)
+   print(n % 2,end = '')
+
+convertToBinary(dec)
+print()
