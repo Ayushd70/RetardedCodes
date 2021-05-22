b4982f79da724d31dd922dac5ce1a8fc26349f4a
Mazzya <yahyamazouarenna@gmail.com>
2021-05-22 22:27:46 +0200

    python : isalnum, isalpha and isdigit string methods added
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/isalnum.py
@@ -0,0 +1,4 @@
+# Python program who returns True if the string is all numbers or alphabetic characters
+text = input("Enter your text: ")
+
+print(text.isalnum())
---
+++ python/isdigit.py
@@ -0,0 +1,8 @@
+# Python program who returns True if the string is all numbers
+
+text = input("Enter your text: ")
+
+if text.isdigit():
+    print("The string contains only numbers")
+else:
+    print("The string doesn't contains only numbers")
