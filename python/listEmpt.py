af75d98955987d48e9e3b7abdf8d4bd3bb7517cd
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-25 19:46:23 +0530

    python: add program to check if the list is empty
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/listEmpt.py
@@ -0,0 +1,19 @@
+# Python Program to Check If a List is Empty
+
+# Method 1
+# Using Boolean operation
+my_list = []
+if not my_list:
+    print("the list is empty")
+    
+# Method 2
+# Using len()
+# my_list = []
+# if not len(my_list):
+#     print("the list is empty")
+
+# Method 3
+# Comparing with []
+# my_list = []
+# if my_list == []:
+#     print("The list is empty")
