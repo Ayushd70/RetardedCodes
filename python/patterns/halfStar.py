38bb7bfc8ea221f1bd5cc11ee4bcfaf0a0ce5b6e
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-27 19:01:07 +0530

    python: patterns: add half star pattern
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/patterns/halfStar.py
@@ -0,0 +1,13 @@
+# Program to print half pyramid using *
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i + 1):
+        print("* ", end="")
+    print("\n")
