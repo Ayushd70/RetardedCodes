5b0279b5acf33f919e5b269923c9f62d5d3dde55
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-28 11:36:59 +0530

    python: add program to find the remainder
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/rem.py
@@ -0,0 +1,10 @@
+# Python Program to find the remainder
+
+# store the numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# find the remainder
+remainder = num1 % num2
+
+print("Remainder is:", remainder)
