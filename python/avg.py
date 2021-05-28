07e15e5e6bad080c0cdfdecf399dc90a66d48a54
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-28 11:21:43 +0530

    python: add program to find the average of three numbers
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/avg.py
@@ -0,0 +1,12 @@
+# Python program to find average of three numbers
+
+# take inputs
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# calculate average
+avg = (num1 + num2 + num3) / 3
+
+# display result
+print("The average of numbers = %0.2f" % avg)
