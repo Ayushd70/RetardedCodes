6681f60589c4243d8b72b121a6e9aa6999fbaacf
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-07 19:05:59 +0530

    python: add program to count the frequency of a substring
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/countFreq.py
@@ -0,0 +1,10 @@
+# Python program to Count number of occurrences of a given substring
+
+# Input
+string = input("Enter the string: ")
+substring = input("Enter the counting string: ")
+
+count = string.count(substring)
+
+# print count
+print("The count is:", count)
