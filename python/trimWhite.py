c8056019874e4e1682788a35b46212c941648040
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-14 19:56:41 +0530

    python: add program to trim whitespace
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/trimWhite.py
@@ -0,0 +1,14 @@
+# Python Program to Trim Whitespace From a String
+
+# Using strip()
+my_string = " Python "
+
+print(my_string.strip())
+
+#Using regular expression
+#import re
+#
+#my_string  = " Hello Python "
+#output = re.sub(r'^\s+|\s+$', '', my_string)
+#
+#print(output)
