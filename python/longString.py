cf347d9523c2f3653b3d7c5c6b1fc2b12830f723
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-14 19:42:37 +0530

    python: add program to print multiline string
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ python/longString.py
@@ -0,0 +1,22 @@
+# Python Program to Create a Long Multiline String
+
+# Using triple quotes
+my_string = '''The only way to
+learn to program is
+by writing code.'''
+
+print(my_string)
+
+# Using parentheses and a single/double quotes
+# my_string = ("The only way to \n"
+#         	"learn to program is \n"
+#         	"by writing code.")
+
+# print(my_string)
+
+# Using \
+# my_string = "The only way to \n" \
+#         	"learn to program is \n" \
+#         	"by writing code."
+
+# print(my_string)
