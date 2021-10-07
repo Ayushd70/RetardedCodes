8adc5680506d2d3e37e5ce4aa656d0f1fc6b1bf6
MuzzammilRafiq <mzmlrafiqcse@gmail.com>
2021-10-07 23:31:40 +0530

    Updated FloydTriangle.java

--- java/patterns/FloydTriangle.java
+++ java/patterns/FloydTriangle.java
@@ -1,3 +1,8 @@
+// Floyd's Triangle
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
 package patterns;
 
 import java.util.Scanner;
@@ -5,11 +10,12 @@ import java.util.Scanner;
 public class FloydTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
         int rows = sc.nextInt();
         int number = 1;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < i + 1; j++) {
-                System.out.print(number + "\t");
+                System.out.printf("%4d", number);;
                 number++;
             }
             System.out.println();
