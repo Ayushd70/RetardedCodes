50880acdbfa913902f3fdf0bb9870ed3d149cc98
Mazzya <41023812+Mazzya@users.noreply.github.com>
2021-10-23 07:25:18 +0200

    python: lambda function (#20)

---
+++ python/lambda.py
@@ -0,0 +1,11 @@
+# Program showing some examples of lambda functions in python
+
+double_number = lambda num: num*2
+
+reverse_string = lambda string: string[::-1]
+
+add_numbers = lambda x,y: x+y
+
+print(double_number(5))
+print(reverse_string("Hi, this is a test !"))
+print(add_numbers(10, 23))
