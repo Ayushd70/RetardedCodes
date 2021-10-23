d6d95438085dd1f9c824c957e68310501d7c8417
Ayush Dubey <ayushdubey70@gmail.com>
2021-10-23 20:38:48 +0530

    java: breath first search
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ java/BreadthFirstSearch.java
@@ -0,0 +1,57 @@
+import java.util.LinkedList;
+
+public class BreadthFirstSearch {
+    private final int V;
+    private final LinkedList[] adj;
+
+    BreadthFirstSearch(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    // Function to add an edge into the graph
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    // prints BFS traversal from a given source s
+    void BFS(int s) {
+        boolean[] visited = new boolean[V];
+
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            for (int n : (Iterable<Integer>) adj[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BreadthFirstSearch g = new BreadthFirstSearch(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Following is Breadth First Traversal " +
+                "(starting from vertex 2)");
+
+        g.BFS(2);
+    }
+}
