c6e3444cb5c0c5d5570e148108ee227db2590f0b
Ayush Dubey <ayushdubey70@gmail.com>
2022-01-21 20:00:08 +0530

    c: move floyd warshall algo into c dir
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- c/floydWarshallAlgo.c
+++ c/floydWarshallAlgo.c
@@ -10,7 +10,8 @@
 void printMatrix(int matrix[][nV]);
 
 // Implementing floyd warshall algorithm
-void floydWarshall(int graph[][nV]) {
+void floydWarshall(int graph[][nV])
+{
   int matrix[nV][nV], i, j, k;
 
   for (i = 0; i < nV; i++)
@@ -18,9 +19,12 @@ void floydWarshall(int graph[][nV]) {
       matrix[i][j] = graph[i][j];
 
   // Adding vertices individually
-  for (k = 0; k < nV; k++) {
-    for (i = 0; i < nV; i++) {
-      for (j = 0; j < nV; j++) {
+  for (k = 0; k < nV; k++)
+  {
+    for (i = 0; i < nV; i++)
+    {
+      for (j = 0; j < nV; j++)
+      {
         if (matrix[i][k] + matrix[k][j] < matrix[i][j])
           matrix[i][j] = matrix[i][k] + matrix[k][j];
       }
@@ -29,9 +33,12 @@ void floydWarshall(int graph[][nV]) {
   printMatrix(matrix);
 }
 
-void printMatrix(int matrix[][nV]) {
-  for (int i = 0; i < nV; i++) {
-    for (int j = 0; j < nV; j++) {
+void printMatrix(int matrix[][nV])
+{
+  for (int i = 0; i < nV; i++)
+  {
+    for (int j = 0; j < nV; j++)
+    {
       if (matrix[i][j] == INF)
         printf("%4s", "INF");
       else
@@ -41,10 +48,11 @@ void printMatrix(int matrix[][nV]) {
   }
 }
 
-int main() {
+int main()
+{
   int graph[nV][nV] = {{0, 3, INF, 5},
-             {2, 0, INF, 4},
-             {INF, 1, 0, INF},
-             {INF, INF, 2, 0}};
+                       {2, 0, INF, 4},
+                       {INF, 1, 0, INF},
+                       {INF, INF, 2, 0}};
   floydWarshall(graph);
 }
