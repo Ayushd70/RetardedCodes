c25bebbedf1e4f4ee2227459831e2c9d7621dafd
Ayush Dubey <ayushdubey70@gmail.com>
2022-01-23 22:38:50 +0530

    java: remove duplicate elements from an array
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ java/RemoveDup.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class RemoveDup {
+    public static void removeDuplicates(int[] arrayWithDuplicates) {
+        System.out.println("Array with Duplicates :");
+
+        for (int i = 0; i < arrayWithDuplicates.length; i++) {
+            System.out.print(arrayWithDuplicates[i] + "\t");
+        }
+        // Assuming all elements in input array are unique
+
+        int noOfUniqueElements = arrayWithDuplicates.length;
+
+        // Comparing each element with all other elements
+
+        for (int i = 0; i < noOfUniqueElements; i++) {
+            for (int j = i + 1; j < noOfUniqueElements; j++) {
+                // If any two elements are found equal
+
+                if (arrayWithDuplicates[i] == arrayWithDuplicates[j]) {
+                    // Replace duplicate element with last unique element
+
+                    arrayWithDuplicates[j] = arrayWithDuplicates[noOfUniqueElements - 1];
+
+                    // Decrementing noOfUniqueElements
+
+                    noOfUniqueElements--;
+
+                    // Decrementing j
+
+                    j--;
+                }
+            }
+        }
+
+        // Copying only unique elements of arrayWithDuplicates into
+        // arrayWithoutDuplicates
+
+        int[] arrayWithoutDuplicates = Arrays.copyOf(arrayWithDuplicates, noOfUniqueElements);
+
+        // Printing arrayWithoutDuplicates
+
+        System.out.println();
+
+        System.out.println("Array Without Duplicates : ");
+
+        for (int i = 0; i < arrayWithoutDuplicates.length; i++) {
+            System.out.print(arrayWithoutDuplicates[i] + "\t");
+        }
+
+        System.out.println();
+
+        System.out.println("==============================");
+    }
+
+    public static void main(String[] args) {
+        removeDuplicates(new int[] { 4, 3, 2, 4, 9, 2 });
+
+        removeDuplicates(new int[] { 1, 2, 1, 2, 1, 2 });
+
+        removeDuplicates(new int[] { 15, 21, 11, 21, 51, 21, 11 });
+
+        removeDuplicates(new int[] { 7, 3, 21, 7, 34, 18, 3, 21 });
+    }
+}
