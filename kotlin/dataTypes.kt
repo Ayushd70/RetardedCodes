2424bff046eb9d0a149c9255f33f1815fb268f0e
Ayush Dubey <ayushdubey70@gmail.com>
2022-02-16 22:52:47 +0530

    kt: data types in kotlin
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ kotlin/dataTypes.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var name: String
+    name = "Ayush"
+
+    var age: Int = 22
+    var myAge = 22
+
+    var isAlive: Boolean = true
+    var marks: Float = 97.4F
+    var percentage: Double = 90.78
+    var gender: Char = 'M'
+
+    print(marks)
+}
