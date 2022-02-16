8699cf672598fcad3fc79391179e3164e626698d
Ayush Dubey <ayushdubey70@gmail.com>
2022-02-16 22:54:01 +0530

    kt: string interpolation
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ kotlin/stringInterpolation.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    val rect = Rectangle()
+    rect.length = 5
+    rect.breadth = 3
+
+    print("The length of the rectangle is ${rect.length} and breadth is ${rect.breadth}. The area is ${rect.length * rect.breadth}")
+
+}
+
+class Rectangle {
+
+    var length: Int = 0
+    var breadth: Int = 0
+}
