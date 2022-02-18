2c91bdcaac472b0e24c4f2fecb458263f62b1c74
Ayush Dubey <ayushdubey70@gmail.com>
2022-02-18 22:28:06 +0530

    kt: for loop in kotlin
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ kotlin/forLoop.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    for (i in 1..10) {
+        if (i % 2 == 0) {
+            println(i)
+        }
+    }
+
+    println()
+
+    for (i in 10 downTo 0) {
+        if (i % 2 == 0) {
+            println(i)
+        }
+    }
+}
