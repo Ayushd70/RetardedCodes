7cb6d7ba4f603facb7ec58b69d8a2242f886570a
Ayush Dubey <ayushdubey70@gmail.com>
2022-02-19 23:30:50 +0530

    kt: break keyword in kotlin
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ kotlin/breakKeyword.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    for (i in 0..4) {
+        println(i)
+
+        if (i == 2) {
+            break
+        }
+    }
+
+    println()
+
+    myLoop@ for (i in 1..3) {
+        for (j in 1..3) {
+            println("$i  $j")
+            if (i == 2 && j == 2)
+                break@myLoop
+        }
+    }
+}
