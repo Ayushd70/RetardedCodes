76a0193436d8fc2a706b61721fa7e9ae1d70d2b0
Ayush Dubey <ayushdubey70@gmail.com>
2022-02-20 22:15:24 +0530

    kt: continue keyword in kotlin
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ kotlin/contKeyword.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    for (i in 1..3) {
+        if (i == 2)
+            continue
+        println(i)
+    }
+
+
+    myLoop@ for (i in 1..3) {
+        for (j in 1..3) {
+            if (i == 2 && j == 2) {
+                continue@myLoop
+            }
+            println("$i  $j")
+        }
+    }
+}
