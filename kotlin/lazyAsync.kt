dc00d8f5e40749f864a818477ebfbd81a7ca0cb9
Ayush Dubey <ayushdubey70@gmail.com>
2022-04-12 22:59:34 +0530

    kt: update lazy async
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- kotlin/lazyAsync.kt
+++ kotlin/lazyAsync.kt
@@ -4,21 +4,21 @@ fun main() = runBlocking {
 
     println("Main program starts: ${Thread.currentThread().name}")  // main thread
 
-    val msgOne: Deferred<String> = async(start = CoroutineStart.LAZY) { getMessageOne() }
-    val msgTwo: Deferred<String> = async(start = CoroutineStart.LAZY) { getMessageTwo() }
-    println("The entire message is: ${msgOne.await() + msgTwo.await()}")
+    val msg: Deferred<Unit> = async(start = CoroutineStart.LAZY) { getMessage() }
+    val msg2: Deferred<Unit> = async(start = CoroutineStart.LAZY) { getMessage2() }
+    println("The entire message is: ${msg.await() + msg2.await()}")
 
     println("Main program ends: ${Thread.currentThread().name}")
 }
 
-suspend fun getMessageOne(): String {
+suspend fun getMessage(): String {
     delay(1000L)
-    println("After working in getMessageOne()")
+    println("After working in getMessage()")
     return "Hello "
 }
 
-suspend fun getMessageTwo(): String {
+suspend fun getMessage2(): String {
     delay(1000L)
-    println("After working in getMessageTwo()")
+    println("After working in getMessage2()")
     return "World!"
 }
