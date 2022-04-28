fd8db8f0b80a89cbb3ae68f7efcc92824bd5d1f1
Ayush Dubey <ayushdubey70@gmail.com>
2022-04-28 21:27:35 +0530

    js: check if a number is even or odd
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/evenOdd.js
@@ -0,0 +1,12 @@
+// program to check if the number is even or odd
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
