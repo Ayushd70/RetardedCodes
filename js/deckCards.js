9c8b934d57ccfad9cea87d4d01e4857f86cc4830
Ayush Dubey <ayushdubey70@gmail.com>
2022-05-11 21:27:57 +0530

    js: shuffle deck of cards
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/deckCards.js
@@ -0,0 +1,45 @@
+// program to shuffle the deck of cards
+
+// declare card elements
+const suits = ["Spades", "Diamonds", "Club", "Heart"];
+const values = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+];
+
+// empty array to contain cards
+let deck = [];
+
+// create a deck of cards
+for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+        let card = { Value: values[x], Suit: suits[i] };
+        deck.push(card);
+    }
+}
+
+// shuffle the cards
+for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+}
+
+console.log('The first five cards are:');
+
+// display 5 results
+for (let i = 0; i < 5; i++) {
+    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
+}
