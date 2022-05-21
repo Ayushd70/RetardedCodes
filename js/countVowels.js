94388fa99db22eb89b8acee8a614c5b8822c6c29
Ayush Dubey <ayushdubey70@gmail.com>
2022-05-21 20:32:04 +0530

    js: count the number of vowels
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/countVowels.js
@@ -0,0 +1,44 @@
+// program to count the number of vowels in a string
+
+// defining vowels
+const vowels = ["a", "e", "i", "o", "u"]
+
+function countVowel(str) {
+    // initialize count
+    let count = 0;
+
+    // loop through string to test if each character is a vowel
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++;
+        }
+    }
+
+    // return number of vowels
+    return count
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+const result = countVowel(string);
+
+console.log(result);
+
+//Using Regex
+
+// function countVowel(str) { 
+
+    // find the count of vowels
+//     const count = str.match(/[aeiou]/gi).length;
+
+    // return number of vowels
+//     return count;
+// }
+
+// take input
+// const string = prompt('Enter a string: ');
+
+// const result = countVowel(string);
+
+// console.log(result);
