b514a0297a4bf578c23c23e3b97789bfd81bae92
Ayush Dubey <ayushdubey70@gmail.com>
2022-05-27 21:37:31 +0530

    js: add key pair to an object
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/addKey.js
@@ -0,0 +1,12 @@
+// program to add a key/value pair to an object
+
+const person = {
+    name: 'Ayush',
+    age: 22,
+    gender: 'male'
+}
+
+// add a key/value pair
+person.height = 6.0;
+
+console.log(person);
