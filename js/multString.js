158536f5ffe20de3752f7f2776783c2bb2936f9f
Ayush Dubey <ayushdubey70@gmail.com>
2022-05-29 22:06:48 +0530

    js: create a multiline string using various methods
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/multString.js
@@ -0,0 +1,17 @@
+// program to create a multiline strings using +
+
+// using the + operator
+const message = 'This is a long message\n' + 
+    'that spans across multiple lines\n' + 
+    'in the code.'
+
+console.log(message);
+
+// program to create a multiline strings using \
+
+// using the \ operator
+// const message = 'This is a long message\n \
+// that spans across multiple lines\n \
+// in the code.'
+
+// console.log(message);
