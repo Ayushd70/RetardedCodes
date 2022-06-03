33027106be7b50fce47f036ce2ec492520e1ecd3
Ayush Dubey <ayushdubey70@gmail.com>
2022-06-03 21:42:22 +0530

    js: trim a string
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/trimStr.js
@@ -0,0 +1,19 @@
+// program to trim a string
+
+const string = '      Hello World       ';
+
+const result = string.trim();
+
+console.log(result);
+
+
+// program to trim a string using regex
+
+// function trimString(x) {
+//     let trimValue = x.replace(/^\s+|\s+$/g,'');
+//     return trimValue;
+// }
+
+// const result = trimString('    Hello world    ');
+// console.log(result);
+
