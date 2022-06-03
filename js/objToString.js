eaebc845ed05e5b1d2f687cee8cff429f46d9839
Ayush Dubey <ayushdubey70@gmail.com>
2022-06-03 21:46:31 +0530

    js: convert objects to strings
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/objToString.js
@@ -0,0 +1,11 @@
+// program to convert an object to a string
+
+const person = {
+    name: 'Ayush',
+    age: 22
+}
+
+const result =  JSON.stringify(person);
+
+console.log(result);
+console.log(typeof result);
