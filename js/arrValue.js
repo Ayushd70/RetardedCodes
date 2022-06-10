780aee04d3bb086cf9d5edc0d1fc8c7480915362
Ayush Dubey <ayushdubey70@gmail.com>
2022-06-10 19:35:41 +0530

    js: check if an array contains a specific value
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/arrValue.js
@@ -0,0 +1,12 @@
+// program to check if an array contains a speified value
+
+const array = ['you', 'will', 'learn', 'javascript'];
+
+const hasValue= array.includes('javascript');
+
+// check the condition
+if(hasValue) {
+    console.log('Array contains a value');
+} else {
+    console.log('Array does not contain a value');
+}
