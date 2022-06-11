47eced6e7855c28304c9b5ad0dad70f55e128e64
Ayush Dubey <ayushdubey70@gmail.com>
2022-06-11 20:09:26 +0530

    js: append an object to an array
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/appendObjArr.js
@@ -0,0 +1,18 @@
+// program to append an object to an array
+
+function insertObject() {
+
+    // append object
+    array.push(obj);
+
+    console.log(arr);
+}
+
+// original array
+let array = [1, 2, 3];
+
+// object to add
+let object = {x:12, y:8};
+
+// call the function
+insertObject(array, object);
