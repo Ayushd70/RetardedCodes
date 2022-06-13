543b678c6b40a9c85cad88b5dc8b6ebe0096ab7f
Ayush Dubey <ayushdubey70@gmail.com>
2022-06-13 20:22:17 +0530

    js: empty an array
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/emptyArr.js
@@ -0,0 +1,16 @@
+// program to empty an array
+
+function emptyArray(arr) {
+
+    // substituting new array
+    arr = [];
+
+    return arr;
+}
+
+const array = [1, 2, 3];
+console.log(array);
+
+// call the function
+const result = emptyArray(array);
+console.log(result);
