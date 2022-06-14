04ae545300cef0cfaee430abcf5cbdbd55b7d56b
Ayush Dubey <ayushdubey70@gmail.com>
2022-06-14 21:16:29 +0530

    js: add element to the start of an array
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/addToStrArr.js
@@ -0,0 +1,14 @@
+// program to add element to start of an array
+
+function addToStart(arr) {
+
+    // adding new array element
+    arr.unshift(4);
+
+    console.log(arr);
+}
+
+ const array = [1, 2, 3];
+
+ // calling the function
+ addToStart(array);
