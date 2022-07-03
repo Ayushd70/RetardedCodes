11b5cfd0cf363e47733f66d6acbf1c552c7a7eaf
Ayush Dubey <ayushdubey70@gmail.com>
2022-07-03 20:20:30 +0530

    js: setTimeOut function passing a parameter
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/setTimeOut.js
@@ -0,0 +1,9 @@
+// program to pass parameter to a setTimeout() function
+
+function hello() {
+    
+    console.log('Hello World!');
+}
+
+setTimeout(hello, 3000); // 3000 is milliseconds(3seconds)
+console.log('This message is shown first');
