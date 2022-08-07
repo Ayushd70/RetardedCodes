8338ce5e5fcb2fb84c8bd87e051b977e5e058c1e
Ayush Dubey <ayushdubey70@gmail.com>
2022-08-07 21:12:32 +0530

    js: cocktail shaker algorithm
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ js/CocktailShaker.js
@@ -0,0 +1,31 @@
+//Cocktail Shaker Sort
+
+let nums = [4, 9, 0, 3, 1, 5];
+console.log("Original array:");
+console.log(nums);
+let is_Sorted = true;
+while (is_Sorted) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      let temp = nums[i];
+      nums[i] = nums[i + 1];
+      nums[i + 1] = temp;
+      is_Sorted = true;
+    }
+  }
+
+  if (!is_Sorted) break;
+
+  is_Sorted = false;
+
+  for (let j = nums.length - 1; j > 0; j--) {
+    if (nums[j - 1] > nums[j]) {
+      let temp = nums[j];
+      nums[j] = nums[j - 1];
+      nums[j - 1] = temp;
+      is_Sorted = true;
+    }
+  }
+}
+console.log("Sorted array:");
+console.log(nums);
