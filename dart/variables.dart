ed44b487930538e2a06825ced6ddccbf9a2080e1
Ayush Dubey <ayushdubey70@gmail.com>
2022-08-17 20:13:14 +0530

    dart: variables in dart
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/variables.dart
@@ -0,0 +1,17 @@
+main() {
+  // var declares a variable and dart analyzer infers the type
+  var a = "dart";
+  print(a);
+
+  // num is a type
+  num b = 42;
+  print(b);
+
+  // final variable cannot be changed once declared
+  final num c = 7;
+  print(c);
+
+  // compile time constant
+  const double d = 7.777;
+  print(d);
+}
