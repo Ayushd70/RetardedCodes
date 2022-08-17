729e207f90364f06eeec628b8c210914288a7b1f
Ayush Dubey <ayushdubey70@gmail.com>
2022-08-17 20:55:52 +0530

    dart: if else in dart
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/ifelse.dart
@@ -0,0 +1,19 @@
+main() {
+  // if-else statement
+  if (7 % 2 == 0) {
+    print('7 is even number');
+  } else {
+    print('7 is odd');
+  }
+
+  // if statement
+  if (8 % 4 == 0) {
+    print('8 is divisble by 4');
+  }
+
+  // ternary operator
+  var isAlive = true;
+  // ignore: dead_code
+  var monday = isAlive ? 'doctor' : 'null';
+  print('monday appointment: $monday');
+}
