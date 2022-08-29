7be4f4515fa3ff2a72ac5d9f86ca624c394322a1
Ayush Dubey <ayushdubey70@gmail.com>
2022-08-29 20:26:59 +0530

    dart: constants in dart
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/constants.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+// compile-time constants are defined using 'const'
+const name = "ayush";
+
+// Objects can also be declared at compile-time
+const Rectangle bounds = const Rectangle(0, 0, 5, 5);
+
+main() {
+  print(name);
+  print(bounds);
+}
