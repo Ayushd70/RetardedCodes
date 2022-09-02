480b2919ccde0d22cd8278dfab40baf94ebbbb90
Ayush Dubey <ayushdubey70@gmail.com>
2022-09-02 21:24:06 +0530

    dart: static members in dart
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/static.dart
@@ -0,0 +1,12 @@
+class Position {
+  // static func
+  static int get maxX => 100;
+
+  // static property
+  static int maxY = 200;
+}
+
+main() {
+  print(Position.maxX);
+  print(Position.maxY);
+}
