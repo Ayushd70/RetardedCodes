57d222b34b86494304be8761db7fccf02629e6b4
Ayush Dubey <ayushdubey70@gmail.com>
2022-09-11 20:48:29 +0530

    dart: comments in dart
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/comments.dart
@@ -0,0 +1,20 @@
+/// Represents a two-dimensional position
+/// has [x] and [y] properties
+///
+/// example code can be defined using a four-space indent:
+///
+///     var p = new Position();
+///
+class Position {
+  /*
+  multi-line comments
+   */
+  int x = 0;
+
+  // A regular comment
+  int y = 0;
+}
+
+main() {
+  print(new Position().runtimeType);
+}
