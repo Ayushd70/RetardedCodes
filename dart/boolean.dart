d8fe2a2d422abe8134726db6ed6762e984d4a2a3
Ayush Dubey <ayushdubey70@gmail.com>
2022-09-16 22:54:46 +0530

    dart: boolean in dart
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/boolean.dart
@@ -0,0 +1,23 @@
+main() {
+  var myString = "Hello World";
+  var myNumber = 2;
+  var myVariable = null;
+
+  if (myString.isEmpty) {
+    print("myString is empty");
+  } else {
+    print("myString is not empty");
+  }
+
+  if (myNumber < 10) {
+    print("myNumber is less than 10");
+  } else {
+    print("myNumber is greater than 10");
+  }
+
+  if (myVariable == null) {
+    print("myVariable is null");
+  } else {
+    print("myVariable is not null");
+  }
+}
