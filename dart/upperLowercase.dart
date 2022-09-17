8fd84e96f2fd378e83f1ca143b2dd49999d0965c
Ayush Dubey <ayushdubey70@gmail.com>
2022-09-17 21:16:53 +0530

    dart: convert all characters of a string to uppercase or lowercase
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/upperLowercase.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+main() {
+  stdout.writeln("Enter a string : ");
+  var str = stdin.readLineSync();
+
+  stdout.writeln("Uppercase conversion : " + str!.toUpperCase());
+  stdout.writeln("Lowercase conversion : " + str.toLowerCase());
+}
