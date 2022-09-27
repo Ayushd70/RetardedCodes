768918d706e41ff3eedccef401dba489efb6bf52
Ayush Dubey <ayushdubey70@gmail.com>
2022-09-27 22:43:29 +0530

    dart: check if a string ends or starts with a substring
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/endStartSub.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  String? mainString;
+  String? subString;
+
+  print("Enter a string : ");
+  mainString = stdin.readLineSync();
+
+  print("Enter the sub string : ");
+  subString = stdin.readLineSync();
+
+  if (mainString!.startsWith(subString!)) {
+    print('"$mainString" starts with "$subString"');
+  } else if (mainString.endsWith(subString)) {
+    print('"$mainString" ends with "$subString"');
+  } else {
+    print('"$mainString" doesn\'t contain "$subString"');
+  }
+}
