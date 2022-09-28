ecd24494938b7e750d9d30040b0e1c0fb15775b3
Ayush Dubey <ayushdubey70@gmail.com>
2022-09-28 12:19:46 +0530

    dart: find factorial of a number recursively
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/factorialRec.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+findFactorial(int no) {
+  if (no == 1) {
+    return 1;
+  }
+  return no * findFactorial(no - 1);
+}
+
+main() {
+  print("Enter a number : ");
+  String? s = stdin.readLineSync();
+  if (s != null) {
+    var no = int.parse(s);
+    print('Factorial of $no is ${findFactorial(no)}');
+  }
+}
