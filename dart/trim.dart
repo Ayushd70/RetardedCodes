b0aeda007f2ec5a993bae624e534b593461ee430
Ayush Dubey <ayushdubey70@gmail.com>
2022-09-29 11:43:24 +0530

    dart: trim a string
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/trim.dart
@@ -0,0 +1,6 @@
+void main() {
+  String message = "      Hello       ";
+  print("***" + message.trimRight() + "***");
+  print("***" + message.trimLeft() + "***");
+  print("***" + message.trim() + "***");
+}
