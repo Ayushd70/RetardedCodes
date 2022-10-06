707ed37b0df75eb04457fb240d9f4d4de963fe67
Arsalan <35004378+a092devs@users.noreply.github.com>
2022-10-06 19:27:52 +0530

    java: longest common prefix
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ java/LongestCommonPrefix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
