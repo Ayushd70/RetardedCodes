f46f49ee3d395f29378298752163284459f8e89c
Ayush Dubey <ayushdubey70@gmail.com>
2022-10-14 12:40:09 +0530

    dart: printing dollar sign
    * using backslash
    * using raw string
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/dollarPrint.dart
@@ -0,0 +1,13 @@
+// using backslash
+main() {
+  var s = "Hello \$ World";
+  print(s);
+}
+
+
+// Using raw string
+// Raw string can be created by prepending an ‘r’ to a string. It treats $ as a literal character.
+// main() {
+//   var s = r'Hello $ World';
+//   print(s);
+// }
