6f7aa4a58457a190ba87bf0ade81a9d224b46e65
Ayush Dubey <ayushdubey70@gmail.com>
2022-10-22 15:02:58 +0530

    dart: convert string to integer
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/stringToInt.dart
@@ -0,0 +1,11 @@
+void main() {
+  int firstInt = int.parse('1');
+  int secondInt = int.parse('10000');
+  int thirdInt = int.parse('-100');
+  int fourthInt = int.parse('+100');
+
+  print("firstInt : $firstInt");
+  print("secondInt : $secondInt");
+  print("thirdInt : $thirdInt");
+  print("fourthInt : $fourthInt");
+}
