d429074565053400adabcb270374ba819bb3c6a9
Ayush Dubey <ayushdubey70@gmail.com>
2022-10-27 12:01:25 +0530

    dart: find the absolute value of a number
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/absVal.dart
@@ -0,0 +1,11 @@
+void main() {
+  var number_one = 10.6;
+  var number_two = -10.6;
+  var number_three = 11;
+  var number_four = -11;
+
+  print(number_one.abs());
+  print(number_two.abs());
+  print(number_three.abs());
+  print(number_four.abs());
+}
