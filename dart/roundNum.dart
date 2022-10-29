90529131b646f10fc2d301c6b90bc9a858d0abad
Ayush Dubey <ayushdubey70@gmail.com>
2022-10-29 10:34:56 +0530

    dart: round a number to its nearest value
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ dart/roundNum.dart
@@ -0,0 +1,11 @@
+void main() {
+  var number_one = 10.6;
+  var number_two = -10.6;
+  var number_three = 11;
+  var number_four = -11.7;
+
+  print(number_one.round());
+  print(number_two.round());
+  print(number_three.round());
+  print(number_four.round());
+}
